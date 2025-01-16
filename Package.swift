870aa4ef4e7c8754c78e5927e67879ad1c65c7a5
Hafiz Ahsan <hafiz@idwise.com>
2025-01-16 18:03:03 +0500

    Changed module name and package name

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "IDWiseSDK",
-            targets: ["IDWiseSDKLib"])
+            targets: ["IDWiseSDK"])
     ],
     dependencies: [
             // Dependencies declare other packages that this package depends on.
@@ -22,7 +22,7 @@ let package = Package(
           // Targets are the basic building blocks of a package. A target can define a module or a test suite.
           // Targets can depend on other targets in this package, and on products in packages this package depends on.
           .binaryTarget(
-              name: "IDWiseSDKLib",
+              name: "IDWiseSDK",
               url: "https://mobile-sdk.idwise.ai/ios/5.1.6/IDWiseSDK.xcframework.zip",
               checksum: "988aff83a10d8165cef6911c3b227040ecf87768bf90d003b8656a89532293f4"
           ),
