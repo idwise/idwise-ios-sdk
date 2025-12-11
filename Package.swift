3d9925be62710c08a3cc9ca1dd2926430b695571
Waqar Zahour <waqar.zahour@idwise.com>
2025-12-11 13:11:36 +0500

    Bump up IDWiseSDK version to 5.9.0

--- Package.swift
+++ Package.swift
@@ -5,16 +5,16 @@ import PackageDescription
 
 let checksumForShield = "3f80bef16ebda425b53a8e9f2e7d4a3ce9b027d8dbcccd057605acca8c3bef83"
 let checksumForFP = "9a9ac84b28395848247b41ce632fd00cbd309c9a99772a9ff0cf2099ef636937"
-let checksumForIDWise = "5f945b6028cca5969698a186e5037a54bea90da13ad2bae9a55316a051975199"
+let checksumForIDWise = "4c550c6a5826a8c6ccf7a08fdc14e84cd292f4beb34ae148f7a6ad04babb61e7"
 
 let shieldVersion = "1-5-52"
 let fpVersion = "2.11.0"
-let idwiseSDKVersion = "5.8.2"
+let idwiseSDKVersion = "5.9.0"
 
 let package = Package(
     name: "IDWise",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v15)
     ],
     products: [
         .library(
