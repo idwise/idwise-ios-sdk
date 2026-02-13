ffa5ad0452039c246fa93b93dd6cd005d542da62
Waqar Zahour <waqar.zahour@idwise.com>
2026-02-13 21:36:22 +0500

    Update checksum for version 6.2.0

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let checksumForShield = "3f80bef16ebda425b53a8e9f2e7d4a3ce9b027d8dbcccd057605acca8c3bef83"
 let checksumForFP = "bc66d7912a0a162c1082381f0f0550e3afaa3d267d805e6a2159ccc0f8f247dd"
-let checksumForIDWise = "5947af379e20b9e63f5c79942c124b779a71321ede6baed9947c92c221575155"
+let checksumForIDWise = "283dd856e379ea29986be7034796580989d00242a717323e44d5e59b634aa064"
 
 let shieldVersion = "1-5-52"
 let fpVersion = "2.12.0"
