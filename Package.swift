b0905b9f115e93fd6bb9336841312eba8661033b
Hafiz Ahsan <hafiz@idwise.com>
2026-02-18 01:05:07 +0500

    bump IDWiseSDK version to 6.2.3

--- Package.swift
+++ Package.swift
@@ -1,15 +1,17 @@
 // swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+// swift-tools-version: 5.10
+
 import PackageDescription
 
 let checksumForShield = "3f80bef16ebda425b53a8e9f2e7d4a3ce9b027d8dbcccd057605acca8c3bef83"
 let checksumForFP = "bc66d7912a0a162c1082381f0f0550e3afaa3d267d805e6a2159ccc0f8f247dd"
-let checksumForIDWise = "283dd856e379ea29986be7034796580989d00242a717323e44d5e59b634aa064"
+let checksumForIDWise = "94552992e067881d2826b3afcd6797ce41370ec22da94cff3dedfff751863461"
 
 let shieldVersion = "1-5-52"
 let fpVersion = "2.12.0"
-let idwiseSDKVersion = "6.2.0"
+let idwiseSDKVersion = "6.2.3"
 
 let package = Package(
     name: "IDWise",
